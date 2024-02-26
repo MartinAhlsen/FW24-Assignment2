c5d80ec10b6a35e19d759210c08d7cec10fe4cf2
MartinAhlsen <martin.ahlsen@gmail.com>
2024-02-26 21:26:24 +0100

    add basic landing page

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
-    res.render()
+    res.render("pages/home",{
+        headTitle: "Utanför boxen",
+    })
 })
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/tastings", tastingsRouter);
-app.use("/lectures", lecturesRouter);
+
+// app.use("/tastings", tastingsRouter);
+// app.use("/lectures", lecturesRouter);
 
 app.listen(port, () => {
     console.log(`Port: ${port}`)
