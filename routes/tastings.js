7d8154051e2e013273c259be1e0ecb39ce4e0e9a
MartinAhlsen <martin.ahlsen@gmail.com>
2024-05-23 13:36:02 +0200

    image path fix

--- routes/tastings.js
+++ routes/tastings.js
@@ -5,62 +5,47 @@ import { beginnerTastings } from "../data/products.js";
 import { advancedTastings } from "../data/products.js";
 import { expertTastings } from "../data/products.js";
 
-const tastingsRouter = express.Router()
+const tastingsRouter = express.Router();
 
 tastingsRouter.get("/", (req, res) => {
-    res.render(
-        "pages/categories",
-        {
-            pageName: "provningar",
-            headTitle: "Provningar",
-            type: "Provningar",
-            hero: "images/tasting-hero.webp",
-            products: tastings,
-            level: "Grund"
-        }
-    )
+  res.render("pages/categories", {
+    pageName: "provningar",
+    headTitle: "Provningar",
+    type: "Provningar",
+    hero: "images/Tasting-hero.webp",
+    products: tastings,
+    level: "Grund",
+  });
 });
 
-tastingsRouter.get("/grund", (req,res) => {
-    res.render(
-        "pages/product",
-        {
-            headTitle: "Grund",
-            type: "Grund",
-            hero: "images/lecture-hero.jpg",
-            products: beginnerTastings,
-            heading: "Provningar på grundnivå",
+tastingsRouter.get("/grund", (req, res) => {
+  res.render("pages/product", {
+    headTitle: "Grund",
+    type: "Grund",
+    hero: "images/lecture-hero.jpg",
+    products: beginnerTastings,
+    heading: "Provningar på grundnivå",
+  });
+});
 
-        }
-    )
-})
+tastingsRouter.get("/avancerad", (req, res) => {
+  res.render("pages/product", {
+    headTitle: "Avancerad",
+    type: "Avancerad",
+    hero: "images/lecture-hero.jpg",
+    products: advancedTastings,
+    heading: "Provningar på avancerad nivå",
+  });
+});
 
-tastingsRouter.get("/avancerad", (req,res) => {
-    res.render(
-        "pages/product",
-        {
-            headTitle: "Avancerad",
-            type: "Avancerad",
-            hero: "images/lecture-hero.jpg",
-            products: advancedTastings,
-            heading: "Provningar på avancerad nivå",
+tastingsRouter.get("/expert", (req, res) => {
+  res.render("pages/product", {
+    headTitle: "Expert",
+    type: "Expert",
+    hero: "images/lecture-hero.jpg",
+    products: expertTastings,
+    heading: "Provningar på expertnivå",
+  });
+});
 
-        }
-    )
-})
-
-tastingsRouter.get("/expert", (req,res) => {
-    res.render(
-        "pages/product",
-        {
-            headTitle: "Expert",
-            type: "Expert",
-            hero: "images/lecture-hero.jpg",
-            products: expertTastings,
-            heading: "Provningar på expertnivå",
-
-        }
-    )
-})
-
-export default tastingsRouter
+export default tastingsRouter;
